23a703b1a284823d6517b35007ed6a228e07588b
Miguel Duque <mduquema@gmail.com>
2023-07-25 10:53:31 -0500

    add marked as 0.1.3 version

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,38 @@
       <h3>Hola</h3>
       <p>Este es el component OlContainer</p>
     </OlContainer>
+    <OlContainer :isFlexCol="true">
+      <div class="w-full px-5 py-3">
+        <OlMarkdownInput
+          :label="'Documento'"
+          :modelValue="markdownInput"
+          @update:modelValue="markdownInput = $event"
+        />
+      </div>
+    </OlContainer>
   </OlContainer>
 </template>
 
 <script>
 import OlContainer from './components/ui/container/index.vue';
+import OlMarkdownInput from './components/forms/MarkdownInput.vue';
+import OlMarkdownDisplay from './components/ui/markdown-display/index.vue';
+
 export default {
   name: 'App',
   components: {
     OlContainer,
+    OlMarkdownInput,
+    OlMarkdownDisplay,
+  },
+  data() {
+    return {
+      category: '',
+      subcategory: '',
+      description: '',
+      markdownInput: '',
+      status: '',
+    };
   },
 };
 </script>
