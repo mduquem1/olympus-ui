58ca2eb4402e1f4d6c3b27bbf9db9121b61a7811
Miguel Duque <mduquema@gmail.com>
2023-08-04 10:27:17 -0500

    fuck this shit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import markdownRawPlugin from 'vite-raw-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    markdownRawPlugin({
+      fileRegex: /\.md$/,
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/olympus-ui.js',
-      name: 'OlympusUI'
+      name: 'olympus-ui',
+      fileName: (format) => `olympus-ui.${format}.js`,
     },
+    mode: 'production',
+    cssCodeSplit: true,
     rollupOptions: {
-      external: ['vue', 'marked'],
+      external: ['vue', 'marked', 'daisy-ui'],
       output: {
+        inlineDynamicImports: true,
         globals: {
           vue: 'Vue',
-          marked: 'marked'
-        }
-      }
-    }
-  }
-})
+          marked: 'marked',
+          daisyui: 'daisyui',
+        },
+      },
+    },
+  },
+});
