4df4efb17a27df3d6de484b6d53bef653c2d616a
Miguel Duque <mduquema@gmail.com>
2023-09-04 11:52:28 -0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
 import markdownRawPlugin from 'vite-raw-plugin';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    ViteFederation({
+    Vue(),
+    markdownRawPlugin({
+      fileRegex: /\.md$/,
+    }),
+    federation({
       name: 'olympus-ui',
       filename: 'main.js',
       exposes: {
+        './OlMarkdownInput': './src/components/forms/MarkdowInput.vue',
+        './OlTextareaInput': './src/components/forms/TextareaInput.vue',
+        './OlTextInput': './src/components/forms/TextInput.vue',
         './OlButton': './src/components/ui/button/index.vue',
+        './OlContainer': './src/components/ui/container/index.vue',
+        './OlLoader': './src/components/ui/loader/index.vue',
+        './OlMarkdownDisplay': './src/components/ui/markdown-display/index.vue',
       },
-    }),
-    markdownRawPlugin({
-      fileRegex: /\.md$/,
     }),
   ],
   build: {
